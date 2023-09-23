b4b023a44d500e79707961ef5089d4b6e99ea182
Caio lima <caiohenrys50@gmail.com>
2023-09-22 23:52:50 -0300

    project

---
+++ src/Project.tsx
@@ -0,0 +1,9 @@
+import { MySwiper } from "./components/swaper";
+
+export default function Project() {
+  return (
+    <div className="  flex flex-col items-center justify-center relative  mb-10">
+        <MySwiper/>
+    </div>
+  )
+}
