2739b993e3417d0b2ce4c655722d554565a7b306
Caio lima <caiohenrys50@gmail.com>
2023-09-28 14:38:11 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import Skills from "./Skills";
 import Footer from "./Footer";
 import BackToTop from "./util";
 import { MdKeyboardDoubleArrowDown } from "react-icons/md";
-import { Project } from "./Project";
 import { useEffect, useState } from "react";
+import Project from "./Project";
 
 export function App() {
 
@@ -32,14 +32,13 @@ export function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="dark:bg-white-200 mx-auto max-w-screen-xl  h-auto p-4 relative ">
           <Header />
-          <div className="flex items-center justify-center ">
-            <div className="w-2 h-80 bg-secondary left-2 top-44 fixed">
-            <span className="absolute top-0 -mt-4 left-3 text-slate-200">0%</span>
-            <span className="absolute top-20 -mt-2 left-3 text-slate-200">25%</span>
-            <span className="absolute top-40 -mt-2 left-3 text-slate-200">50%</span>
-            <span className="absolute top-60 -mt-2 left-3 text-slate-200">75%</span>
-            <span className="absolute top-80 -mt-2 left-3 text-slate-200">100%</span>
-
+              <div className="flex items-center justify-center">
+                <div className="w-2 h-80 bg-secondary left-2 top-44 fixed">
+                <span className="absolute top-0 -mt-4 left-3 text-slate-200">0%</span>
+                <span className="absolute top-20 -mt-2 left-3 text-slate-200">25%</span>
+                <span className="absolute top-40 -mt-2 left-3 text-slate-200">50%</span>
+                <span className="absolute top-60 -mt-2 left-3 text-slate-200">75%</span>
+                <span className="absolute top-80 -mt-2 left-3 text-slate-200">100%</span>
               <div  style={{ height:`${scrollTop}rem`, width: "4px" , }} className="z-50 bg-primary flex rounded-full "/>
             </div>
           </div>
@@ -47,8 +46,8 @@ export function App() {
           <MdKeyboardDoubleArrowDown  className=" w-full text-5xl mb-5" />
           <NavBar />
           <About />
-          <Skills />
           <Project />
+          <Skills />
           <Footer/>
           <BackToTop/>
         </div>
