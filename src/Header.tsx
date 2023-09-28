0a50602cba5f006a998cd1934c889ff28d03ced1
Caio lima <caiohenrys50@gmail.com>
2023-09-28 14:38:50 -0300

    header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,19 +1,23 @@
 import Redes from "./components/ui/Redes";
 import { ModeToggle } from "./components/mode-toggle";
-import { Separator } from "./components/ui/separator";
 
 export default function Header() {
   return (
-    <div className="h-3 p-8 flex items-center justify-between border-b relative z-99">
-      <h1 className="text-xl font-bold">Caio Lima</h1>
-      <div>
-        <Redes />
-      </div>
-      <div className="flex items-center gap-3">
-        <span className="text-sm text-muted-foreground"></span>
-        <Separator className="h-10" orientation="vertical" />
-        <ModeToggle />
+    <div className="p-4 h-1 sm:p-8 flex items-center justify-between border-b relative z-99 " id="start">
+    <h1 className="text-2xl sm:text-3xl font-bold">CL<span className="text-secondary ">D</span>S</h1>
+    <div className="relative">
+      <div className="mt-6 sm:mt-16 w-40 sm:w-96 h-16 bg-secondary/5 rounded-full border border-zinc-200 relative ">
+        <div className="absolute "></div>
+        <div className="relative z-10 flex items-center justify-center">
+          <Redes/>
+        </div>
       </div>
     </div>
+    <div className="flex items-center gap-3">
+      <span className="text-sm text-muted-foreground"></span>
+      <ModeToggle />
+    </div>
+  </div>
+  
   );
 }
