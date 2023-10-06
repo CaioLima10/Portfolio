59ec3a8922b718dc9ae502f0cfba3cc06377a63e
Caio lima <caiohenrys50@gmail.com>
2023-10-06 16:30:12 -0300

    add tabs

--- src/Project.tsx
+++ src/Project.tsx
@@ -18,16 +18,18 @@ export default function Project() {
 
   return (
     <>
-      <div data-aos="fade-down"  data-aos-anchor-placement="center-bottom" className='w-full flex items-center justify-center  mb-28'>
+      <div data-aos="fade-down"  data-aos-anchor-placement="center-bottom" className='w-full flex items-center justify-center mt-28  mb-28'>
           <RxDoubleArrowDown 
             size={32}/>
       </div>
-      <div className="bg-gradient-to-r from-black via-gray-900 to-black">
-        <div className="h-3/4"> 
-        <h1 className='w-full flex items-center justify-center text-5xl p-4'  id="project">{t("missions")}</h1>
+      <div className=" flex items-center justify-center bg-gradient-to-r from-black via-gray-900 to-black">
+        <div className="w-full md:max-w-screen h-3/4 "> 
+        <div className=''>
+          <h1 className=' flex items-center justify-center text-4xl  md:text-5xl p-4'  id="project">{t("missions")}</h1>
+        </div>
 
-          <Tabs defaultValue="firstProject" className="w-full p-16 mb-28  flex items-center justify-center flex-col">
-            <TabsList className="grid w-full grid-cols-4">
+          <Tabs defaultValue="firstProject" className="w-full flex flex-col items-center justify-center p-16 mb-28 ">
+            <TabsList className="grid w-72 md:w-full grid-cols-4">
               <TabsTrigger value="firstProject">{t("title-project")} 1</TabsTrigger>
               <TabsTrigger value="secondProject">{t("title-project")} 2</TabsTrigger>
               <TabsTrigger value="thirdProject">{t("title-project")} 3</TabsTrigger>
